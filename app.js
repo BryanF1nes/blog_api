7f1c015c3504990e0fec1099277a1398438d1463
Bryan <bryanfinesw@gmail.com>
2025-05-17 16:05:05 -0400

    added passport local strategy

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
 import 'dotenv/config';
 import express from 'express';
+import passport from 'passport';
+import bcrypt from 'bcryptjs';
+import { Strtegy as LocalStrategy } from 'passport-local';
 import postRouter from './routes/postRouter.js';
 import accountRouter from './routes/accountRouter.js';
+import prisma from './utils/prisma.js';
 
 const app = express();
+
+passport.use(passport.session());
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findFirstOrThrow({
+        where: {
+          username: username
+        }
+      })
+
+      if (!user) return done(null, false, { message: "Incorrect username" });
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) return done(null, false, { message: "Incorrect password" });
+
+      return done(null, user)
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findFirstOrThrow({
+      where: {
+        id: id
+      }
+    })
+
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
