93bfc1dcda41216313c5d97b3e6d60967440136c
Bryan <bryanfinesw@gmail.com>
2025-05-18 11:08:13 -0400

    fixing for jwt

--- app.js
+++ app.js
@@ -1,57 +1,20 @@
 import 'dotenv/config';
 import express from 'express';
+import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken';
 import passport from 'passport';
+import { Strategy as jwtStrategy } from 'passport-jwt';
 import bcrypt from 'bcryptjs';
-import { Strtegy as LocalStrategy } from 'passport-local';
 import postRouter from './routes/postRouter.js';
 import accountRouter from './routes/accountRouter.js';
 import prisma from './utils/prisma.js';
 
 const app = express();
 
-passport.use(passport.session());
+passport.use(jwtStrategy);
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await prisma.user.findFirstOrThrow({
-        where: {
-          username: username
-        }
-      })
-
-      if (!user) return done(null, false, { message: "Incorrect username" });
-
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) return done(null, false, { message: "Incorrect password" });
-
-      return done(null, user)
-    } catch (error) {
-      return done(error);
-    }
-  })
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await prisma.user.findFirstOrThrow({
-      where: {
-        id: id
-      }
-    })
-
-    done(null, user);
-  } catch (error) {
-    done(error);
-  }
-});
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('api/v1/account/', accountRouter);
 app.use('/api/v1/posts', postRouter);
